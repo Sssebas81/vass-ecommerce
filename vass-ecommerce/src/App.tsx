aab3958835b93e20bde6c92511123f97364b3f75
Ana García <anamgarcia1401@gmail.com>
2025-10-14 14:06:27 -0500

    feat: add OurProducts section

--- vass-ecommerce/src/App.tsx
+++ vass-ecommerce/src/App.tsx
@@ -1,12 +1,14 @@
 import Categories from "./components/categories/Categories";
 import Heropage from "./components/hero/Heropage";
 import Navbar from "./components/navbar/Navbar";
+import OurProducts from "./components/ourproducts/OurProducts";
 function App() {
   return (
     <div>
       <Navbar />
       <Heropage/>
       <Categories/>
+      <OurProducts/>
     </div>
   );
 }
