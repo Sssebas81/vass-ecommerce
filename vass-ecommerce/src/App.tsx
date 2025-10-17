b3b9e8c5252e866552c850a0d5bffd43f5b12221
Ana García <anamgarcia1401@gmail.com>
2025-10-16 21:11:49 -0500

    feat: add Footer in App

--- vass-ecommerce/src/App.tsx
+++ vass-ecommerce/src/App.tsx
@@ -3,6 +3,7 @@ import Heropage from "./components/hero/Heropage";
 import Navbar from "./components/navbar/Navbar";
 import OurProducts from "./components/ourproducts/OurProducts";
 import SetupIdeas from "./components/setupideas/SetupIdeas";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
       <Categories/>
       <OurProducts/>
       <SetupIdeas/>
+      <Footer/>
     </div>
   );
 }
