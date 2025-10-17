fae0a7072fcd4d9209fc3f9e0f554308848ddc02
Ana García <anamgarcia1401@gmail.com>
2025-10-16 22:13:20 -0500

    feat: add routes

--- vass-ecommerce/src/main.tsx
+++ vass-ecommerce/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home/Home.tsx'
+const Routes=createBrowserRouter([{
+path : '/',
+  element: <Home/>
 
+}])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Routes} />
   </StrictMode>,
 )
