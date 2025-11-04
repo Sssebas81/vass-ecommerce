a120cad35c747f1a52ba099d3af623ecaeede5e4
Sofia Acosta <sacostag06@gmail.com>
2025-11-04 09:11:17 -0500

    feat: add Contact hero section

--- vass-ecommerce/src/pages/contact/Contact.tsx
+++ vass-ecommerce/src/pages/contact/Contact.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/navbar/Navbar";
-import Footer from  "../../components/footer/Footer"
+import Footer from  "../../components/footer/Footer";
+import ContactHero from "../../components/contacthero/ContactHero";
 
 function Contact() {
     return (
         <div>
             <Navbar />
+            <ContactHero/>
             <Footer />
         </div>
     );
