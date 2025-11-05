8ad184ed192abea93d5c7305a02f032cf70fac8b
Sssebas81 <jsebastianortiz81@gmail.com>
2025-11-05 12:04:22 -0500

    feat: navbar and footer in all categories page

---
+++ vass-ecommerce/src/pages/Categories/Devices.tsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+
+function DevicesPage() {
+  return (
+    <div>
+      <Navbar />
+
+      <Footer/>
+    </div>
+  );
+}
+
+export default DevicesPage;
+ 
