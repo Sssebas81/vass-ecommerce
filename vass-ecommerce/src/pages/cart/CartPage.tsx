923f38e802c243bff597bf52bbbd07bab2b6576f
Tu Nombre <anamgarcia1401@gmail.com>
2025-11-27 22:49:40 -0500

    file: creted LikeSlice.tsx

--- vass-ecommerce/src/pages/cart/CartPage.tsx
+++ vass-ecommerce/src/pages/cart/CartPage.tsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Footer from  "../../components/footer/Footer";
 import Cart from "../../components/cart/Cart";
 import CartHero from "../../components/carthero/CartHero";
-import FeaturesBar from "../../components/FeaturesBar/FeaturesBar";
+import FeaturesBar from "../../components/featuresBar/FeaturesBar";
 
 
 function CartPage() {
